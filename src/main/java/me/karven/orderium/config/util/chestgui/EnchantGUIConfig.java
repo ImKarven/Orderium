3d636967a19c47a2c427bc6372ac7a4432c45408
Karven <karven@not-a.mail>
2026-06-11 18:38:05 +0700

    add display item slot option for enchant gui

--- src/main/java/me/karven/orderium/config/util/chestgui/EnchantGUIConfig.java
+++ src/main/java/me/karven/orderium/config/util/chestgui/EnchantGUIConfig.java
@@ -15,11 +15,11 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.util.List;
 
-// TODO: Add a slot config for the display item
 public class EnchantGUIConfig extends GUIConfigFile {
     public boolean enabled;
     public String title;
     public int rows;
+    public int displayItemSlot;
     public final @NotNull EnchantmentConfig enchantmentConfig = new EnchantmentConfig("enchantment-book");
     public final @NotNull ButtonConfig confirmButton = new ButtonConfig("buttons.confirm");
 
@@ -33,6 +33,7 @@ public class EnchantGUIConfig extends GUIConfigFile {
         enabled = config.getBoolean("enabled");
         title = config.getString("title");
         rows = config.getInteger("rows");
+        displayItemSlot = config.getInteger("display-item-slot");
         enchantmentConfig.reload(config);
         confirmButton.reload(config);
     }
@@ -42,6 +43,7 @@ public class EnchantGUIConfig extends GUIConfigFile {
         config.set("enabled", enabled);
         config.set("title", title);
         config.set("rows", rows);
+        config.set("display-item-slot", displayItemSlot);
         enchantmentConfig.save(config);
         confirmButton.save(config);
     }
@@ -51,6 +53,7 @@ public class EnchantGUIConfig extends GUIConfigFile {
         config.addDefault("enabled", enabled);
         config.addDefault("title", title);
         config.addDefault("rows", rows);
+        config.addDefault("display-item-slot", displayItemSlot);
         enchantmentConfig.setDefault(config);
         confirmButton.setDefault(config);
     }
@@ -59,7 +62,8 @@ public class EnchantGUIConfig extends GUIConfigFile {
     public void migrateV5(@NotNull ConfigFile oldConfig) {
         enabled = oldConfig.getBoolean("enchantments");
         title = oldConfig.getString("gui.enchant-item.title");
-        rows = -1;
+        rows = 4;
+        displayItemSlot = 0;
         enchantmentConfig.migrateV5(oldConfig);
         confirmButton.migrateV5(oldConfig, "gui.enchant-item.confirm-button", 0);
         save();
@@ -74,7 +78,8 @@ public class EnchantGUIConfig extends GUIConfigFile {
     public void applyDefaultValues() {
         enabled = false;
         title = "Enchant Your Item";
-        rows = -1;
+        rows = 4;
+        displayItemSlot = 0;
         confirmButton.slot = 8;
         confirmButton.itemStack = ItemStack.of(Material.LIME_WOOL);
         confirmButton.itemStack.editMeta(meta -> {
