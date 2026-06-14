e23f5d3cd5551fa7cdebf3bd1cd85a2931c1584c
Karven <karven@atomicmail.io>
2026-06-14 13:16:14 +0700

    bump faststats version to 0.26.1

--- src/main/java/me/karven/orderium/Orderium.java
+++ src/main/java/me/karven/orderium/Orderium.java
@@ -27,6 +27,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.TimeUnit;
+
 import static me.karven.orderium.config.Config.config;
 
 public final class Orderium extends JavaPlugin {
