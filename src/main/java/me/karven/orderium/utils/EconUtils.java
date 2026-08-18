a4ef0c52ed6ba8c983d93d5bd371e1c996826501
Karven <karven@atomicmail.io>
2026-08-18 21:48:47 +0700

    fix: add proper locks to balance changes

--- src/main/java/me/karven/orderium/utils/EconUtils.java
+++ src/main/java/me/karven/orderium/utils/EconUtils.java
@@ -3,25 +3,39 @@ package me.karven.orderium.utils;
 import me.karven.orderium.obj.MoneyTransaction;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static me.karven.orderium.Orderium.plugin;
 
 public class EconUtils {
+    private static final ConcurrentHashMap<UUID, Object> LOCKS = new ConcurrentHashMap<>();
+
+    private static Object getLock(final @NotNull UUID uuid) {
+        return LOCKS.computeIfAbsent(uuid, _ -> new Object());
+    }
+
     public static void addMoney(OfflinePlayer p, double amount) {
-        final MoneyTransaction transaction = new MoneyTransaction(p, amount);
-        logTransactionBefore(transaction);
-        plugin.getEconomy().depositPlayer(p, amount);
-        logTransactionAfter(transaction);
+        synchronized (getLock(p.getUniqueId())) {
+            final MoneyTransaction transaction = new MoneyTransaction(p, amount);
+            logTransactionBefore(transaction);
+            plugin.getEconomy().depositPlayer(p, amount);
+            logTransactionAfter(transaction);
+        }
     }
 
     /// Returns `true` if the player has enough money to remove, otherwise `false`
     public static boolean removeMoney(Player p, double amount) {
-        if (plugin.getEconomy().getBalance(p) < amount) return false;
-        final MoneyTransaction transaction = new MoneyTransaction(p, amount);
-        logTransactionBefore(transaction);
-        plugin.getEconomy().withdrawPlayer(p, amount);
-        logTransactionAfter(transaction);
-        return true;
+        synchronized (getLock(p.getUniqueId())) {
+            if (plugin.getEconomy().getBalance(p) < amount) return false;
+            final MoneyTransaction transaction = new MoneyTransaction(p, amount);
+            logTransactionBefore(transaction);
+            plugin.getEconomy().withdrawPlayer(p, amount);
+            logTransactionAfter(transaction);
+            return true;
+        }
     }
 
     private static void logTransactionBefore(final MoneyTransaction transaction) {
