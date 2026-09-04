27ece4fe9ea11b21b641ced74b951b676ef0e53c
ImKarven <152170712+ImKarven@users.noreply.github.com>
2026-09-04 10:20:45 +0700

    fix: weird typo

--- src/main/java/me/karven/orderium/guiframework/InventoryGUI.java
+++ src/main/java/me/karven/orderium/guiframework/InventoryGUI.java
@@ -72,7 +72,7 @@ public class InventoryGUI implements InventoryHolder {
 
     public void addItem(@NotNull InventoryItem item, int slot) {
         Preconditions.checkArgument(slot >= 0 && slot < rows * 9, "Slot must be between 0 and " + (rows * 9 - 1) + ", found " + slot);
-        Preconditions.checkArgument(cancelGlobalClickAndDrags, "Items cannot be added to a cancelling clicks and drags GUI.");
+        Preconditions.checkArgument(cancelGlobalClickAndDrags, "Items can only be added to a cancelling clicks and drags GUI.");
         items.put(slot, item);
         handle.setItem(slot, item.getItem());
     }
